9534f13513d913308949a7cbf8947227b29fe101
Dariusz Szyca <39349417+vshyc@users.noreply.github.com>
2018-08-29 22:22:40 +0200

    Add files via upload

---
+++ Inharitance/src/Darek/Main.java
@@ -0,0 +1,14 @@
+package Darek;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Outlander outlander= new Outlander(23);
+    outlander.accalerate(10);
+    outlander.steer(15);
+    outlander.accalerate(20);
+    outlander.accalerate(-19);
+
+
+    }
+}
